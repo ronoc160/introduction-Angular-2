bc7927c44285e32199baabc6d1e60ee80a7723fd
conor <conor@tuimedia.com>
2016-05-25 10:33:26 +0100

    first look

---
+++ app/ytube.service.ts
@@ -0,0 +1,25 @@
+import {Injectable} from 'angular2/core';
+import {Http, Response} from 'angular2/http';
+import {Ytube} from './video';
+import {Observable} from 'rxjs/Observable';
+
+@Injectable()
+export class YtubeService {
+
+    constructor (private http: Http){}
+
+    private ytubeURL = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q=tutsplus&type=video&key=AIzaSyB78DMJw2MJx8hPnSSxhwOr8kaQcTE_mko';
+
+    getVideos() {
+        return this.http.get(this.ytubeURL)
+            .map(res => <Ytube[]> res.json().items)
+            .do(data => console.log(data))
+            .catch(this.errorHandler);
+    }
+    private errorHandler (error: Response) {
+        console.error(error);
+        return Observable.throw(error.json().error || 'Server error');
+    }
+
+
+}
